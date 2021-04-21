4aa657ce06ee374c7c324ad9ebe3ad13e35309b7
Lucas Luna <lukitaass2013@gmail.com>
2021-04-21 17:15:20 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Te muestro el mensaje");
+console.log("Te muestro otro mensaje");
+console.log("Lo monitorea nodemon");
+console.log("Lo monitorea nodemon 2 el regreso");
+console.log("Lo monitorea nodemon 3 el regreso");
+console.log("1 cambio");
